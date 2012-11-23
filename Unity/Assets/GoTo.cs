be479217387723ad02a2fb126adf40c5f4307e0b
sylafrs <sylaf@hotmail.fr>
2012-11-23 17:06:04 +0100

    test arrache

---
+++ Unity/Assets/GoTo.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using System.Collections;
+
+public delegate void callback();
+
+public class GoTo : MonoBehaviour {
+
+    bool sent = false;
+    callback whenArrived;
+    Transform target;
+
+    public float speed;
+
+	// Update is called once per frame
+	void Update () {
+	    if(sent) {
+            Vector3 movement = target.position - this.transform.position;
+            if (movement.magnitude > speed * Time.deltaTime)
+            {
+                movement = movement / movement.magnitude * speed * Time.deltaTime;
+            }
+
+            this.transform.position += movement;
+            if (this.transform.position == target.position)
+            {
+                sent = false;
+                this.transform.parent = target;
+                if(whenArrived != null) whenArrived();
+            }
+
+        }
+	}
+
+    public void sendTo(Transform target, callback whenArrived) {
+        sent = true;
+        this.whenArrived = whenArrived;
+        this.target = target;
+        this.transform.parent = null;
+    }
+}
---
+++ Unity/Assets/rotateMe.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class rotateMe : MonoBehaviour {
+
+    float stillToRotate = 0;
+    Vector3 axis;
+    public float rotateAmount = 1;
+
+    public void rotate(Vector3 axis, float angle)
+    {
+        this.axis = axis;
+        this.stillToRotate = angle/180*Mathf.PI;
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (stillToRotate > 0)
+        {
+            float toRotate = rotateAmount;
+            if (stillToRotate <= rotateAmount)
+            {
+                toRotate = stillToRotate;
+            }
+
+            this.transform.RotateAround(axis, toRotate);
+            stillToRotate -= toRotate;
+        }
+	}
+}
