6b96ca395971499ce18bcc624c45b135afe097ba
sylafrs <sylaf@hotmail.fr>
2012-12-14 12:36:08 +0100

    menu

--- Unity/Assets/Scripts/Settings/InputSettings.cs
+++ Unity/Assets/Scripts/Settings/InputSettings.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 using System.Collections;
 
 public static class InputSettings  {
-    public static KeyCode up;
-    public static KeyCode down;
-    public static KeyCode left;
-    public static KeyCode right;
+    public static KeyCode up = KeyCode.UpArrow;
+    public static KeyCode down = KeyCode.DownArrow;
+    public static KeyCode left = KeyCode.LeftArrow;
+    public static KeyCode right = KeyCode.RightArrow;
 }
 
