da8ecf3d9633454e206a51b5b6c0c579f9a4e6f4
sylafrs <sylaf@hotmail.fr>
2012-12-14 16:34:17 +0100

    gogogo (states)

--- Unity/Assets/Scripts/TEST.cs
+++ Unity/Assets/Scripts/TEST.cs
@@ -3,26 +3,36 @@ using System.Collections;
 
 public class TEST : MonoBehaviour {
 
-    public Unit agent;
+    public Unit agent1;
+    public Unit agent2;
     public float epsilon;
 
 	// Update is called once per frame
 	void Update () {
-        if (agent && Input.GetMouseButtonUp(0))
+        if (agent1 && Input.GetMouseButtonUp(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit outinfo;
-            if (Physics.Raycast(ray, out outinfo, Mathf.Infinity))
-            {
-                if (outinfo.collider.name.Contains("Joueur"))
-                {
-                    agent.ChangeOrder(Order.OrderFollow(outinfo.collider.GetComponent<Unit>(), Order.TYPE_DEPLACEMENT.MARCHE));
-                }
-                if (outinfo.collider.name.Equals("Gazon"))
-                {                    
-                    agent.ChangeOrder(Order.OrderMove(outinfo.point, Order.TYPE_DEPLACEMENT.MARCHE));                  
-                }
-            }             
+            UpdateAgent(agent1);          
+        }
+        if (agent1 && Input.GetMouseButtonUp(1))
+        {
+            UpdateAgent(agent2);
         }
 	}
+
+    void UpdateAgent(Unit agent)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit outinfo;
+        if (Physics.Raycast(ray, out outinfo, Mathf.Infinity))
+        {
+            if (outinfo.collider.name.Contains("Joueur"))
+            {
+                agent.ChangeOrder(Order.OrderFollow(outinfo.collider.GetComponent<Unit>(), Order.TYPE_DEPLACEMENT.MARCHE));
+            }
+            if (outinfo.collider.name.Equals("Gazon"))
+            {
+                agent.ChangeOrder(Order.OrderMove(outinfo.point, Order.TYPE_DEPLACEMENT.MARCHE));
+            }
+        }
+    }
 }
