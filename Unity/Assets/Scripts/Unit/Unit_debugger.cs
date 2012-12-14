0191cafffd0da212450f7e43673a5b32d1619234
sylafrs <sylaf@hotmail.fr>
2012-12-14 16:21:12 +0100

    gogogo (states)

--- Unity/Assets/Scripts/Unit/Unit_debugger.cs
+++ Unity/Assets/Scripts/Unit/Unit_debugger.cs
@@ -5,4 +5,33 @@ using System.Collections;
 public class Unit_debugger : MonoBehaviour {
     public Unit unit;
 
+    public Rect r;
+    public float yy;
+
+    void OnGUI()
+    {
+        if (unit == null) return;
+
+        Rect rr = new Rect(r);
+        string str;
+
+        str = "Ordre : " + unit.GetOrder().type.ToString();
+        GUI.Label(new Rect(rr.x, rr.y, rr.width, rr.height), str);
+        rr.y += yy;
+
+        switch (unit.GetOrder().type)
+        {
+            case Order.TYPE.DEPLACER:
+                str = "Point : " + unit.GetOrder().point.ToString();
+                GUI.Label(new Rect(rr.x, rr.y, rr.width, rr.height), str);
+                rr.y += yy;
+                break;
+
+            case Order.TYPE.SUIVRE:
+                str = "Cible : " + unit.GetOrder().target.name;
+                GUI.Label(new Rect(rr.x, rr.y, rr.width, rr.height), str);
+                rr.y += yy;
+                break;
+        }
+    }
 }
