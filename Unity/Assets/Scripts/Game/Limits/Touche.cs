c738efc49bcc4c9f999f5ed44d983679f285341e
sylafrs <sylaf@hotmail.fr>
2013-01-13 22:34:56 +0100

    touche : projection sur la ligne

--- Unity/Assets/Scripts/Game/Limits/Touche.cs
+++ Unity/Assets/Scripts/Game/Limits/Touche.cs
@@ -9,14 +9,38 @@ using System.Collections;
 [AddComponentMenu("Triggers/Game/Touche")]
 public class Touche : TriggeringTrigger
 {
-    public Transform LigneTouche;
+    public Transform a, b;
+
+    public bool remiseAuCentre = false;
 
     public override void Entered(Triggered t)
     {
-        Ball b = t.GetComponent<Ball>();
-        if (b != null)
+        Ball ball = t.GetComponent<Ball>();
+        if (ball != null)
         {
-            b.setPosition(Vector3.zero);
+            if (a == null || b == null)
+            {
+                Debug.Log("Touche : [Replace au centre]");
+                ball.setPosition(Vector3.zero);
+            }
+            else
+            {
+                Debug.Log("Touche : [Replace au centre, sur la ligne]");
+
+                Vector3 pos = Vector3.Project(ball.transform.position - a.position, b.position - a.position) + a.position;
+                pos.x = remiseAuCentre ? 0 : pos.y; // Au centre
+                pos.y = 0; // A terre
+                ball.setPosition(pos);
+
+            }           
         }
+    }
+    void OnDrawGizmos()
+    {
+        if (a == null || b == null)
+            return;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(a.position, b.position);
     }
 }
