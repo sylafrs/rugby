e110deabe102f39e20e4b1573d3ac2d1ee0fdf93
sylafrs <sylaf@hotmail.fr>
2013-01-28 15:06:12 +0100

    clean mappage

--- Unity/Assets/Scripts/Game/Gamer.cs
+++ Unity/Assets/Scripts/Game/Gamer.cs
@@ -63,26 +63,140 @@ public class Gamer : MonoBehaviour
     List<Unit> unitsSide;
     
 	void Update () {
-        Vector3 direction = Vector3.zero;
-		
-		if (!canMove) return;		
+       
         if (inputs == null) return;
         
-        // Button : check release.
         GamePadState pad = GamePad.GetState(playerIndex);
-        if (pad.IsConnected)
+        
+        CheckReleased(pad);
+        UpdateMOVE(pad);
+        UpdateMOVE(pad);
+        UpdateTACKLE(pad);
+        UpdatePASS(pad);
+	}
+
+    void UpdatePASS(GamePadState pad)
+    {
+        if (game.Ball.Owner == controlled)
         {
-            if (!InputSettingsXBOX.GetButton(game.settings.XboxController.drop, pad))
+            if (pad.IsConnected)
             {
-                btnDropReleased = true;
+                if (btnPassReleased && InputSettingsXBOX.GetButton(game.settings.XboxController.passRight, pad))
+                {
+                    btnPassReleased = false;
+                    UpdatePASS_OnPress(true);                    
+                }
+                else if (btnPassReleased && InputSettingsXBOX.GetButton(game.settings.XboxController.passLeft, pad))
+                {
+                    btnPassReleased = false;
+                    UpdatePASS_OnPress(false);
+                }
+                else if (      !btnPassReleased 
+                            && !InputSettingsXBOX.GetButton(game.settings.XboxController.passRight, pad) 
+                            && !InputSettingsXBOX.GetButton(game.settings.XboxController.passLeft, pad))
+                {
+                    btnPassReleased = true;
+                    UpdatePASS_OnRelease();
+                }
             }
-            if (!InputSettingsXBOX.GetButton(game.settings.XboxController.plaquer, pad))
+            else
             {
-                btnPlaquerReleased = true;
+                if (Input.GetKeyDown(inputs.passRight))
+                {
+                    UpdatePASS_OnPress(true);
+                }
+
+                else if (Input.GetKeyDown(inputs.passLeft))
+                {
+                    UpdatePASS_OnPress(false);
+                }
+
+                else if (Input.GetKeyUp(inputs.passRight) || Input.GetKeyUp(inputs.passLeft))
+                {
+                    UpdatePASS_OnRelease();
+                }
             }
         }
 
-        // Move
+        if (onActionCapture)
+        {
+            timeOnActionCapture += Time.deltaTime;
+        }
+        else
+        {
+            timeOnActionCapture = 0.0f;
+        }
+    }
+
+    void UpdatePASS_OnPress(bool right)
+    {
+        onActionCapture = true;
+
+        if (right)
+        {
+            passDirection = this.transform.right;
+            game.Ball.transform.position = controlled.BallPlaceHolderRight.transform.position;
+            unitsSide = controlled.Team.GetRight(controlled);
+        }
+        else
+        {
+            passDirection = -this.transform.right;
+            game.Ball.transform.position = controlled.BallPlaceHolderLeft.transform.position;
+            unitsSide = controlled.Team.GetLeft(controlled);
+        }       
+    }  
+
+    void UpdatePASS_OnRelease()
+    {
+        if (controlled == game.Ball.Owner)
+        {
+            onActionCapture = false;
+            if (timeOnActionCapture > game.settings.maxTimeHoldingPassButton)
+                timeOnActionCapture = game.settings.maxTimeHoldingPassButton;
+            //Debug.DrawRay(this.transform.position, passDirection, Color.red);
+
+            if (unitsSide.Count != 0)
+            {
+                int unit = Mathf.FloorToInt(unitsSide.Count * timeOnActionCapture / game.settings.maxTimeHoldingPassButton);
+                Debug.Log(unit);
+
+                if (unit == unitsSide.Count) unit--;
+                Unit u = unitsSide[unit];
+
+                controlled.Order = Order.OrderPass(u);
+                passDirection = Vector3.zero;
+            }
+        }
+        else
+        {
+            // Changer de perso.
+        }
+    }
+
+    void UpdateTACKLE(GamePadState pad)
+    {
+       if (pad.IsConnected)
+        {
+            if (btnPlaquerReleased && InputSettingsXBOX.GetButton(game.settings.XboxController.plaquer, pad))
+            {
+                btnPlaquerReleased = false;
+                if (controlled.NearUnits.Count > 0)
+                {
+                    controlled.Order = Order.OrderPlaquer(controlled.NearUnits[0]);
+                }
+            }
+        }
+        else if (Input.GetKeyDown(inputs.plaquer) && controlled.NearUnits.Count > 0)
+        {
+            controlled.Order = Order.OrderPlaquer(controlled.NearUnits[0]);
+        }
+    }
+
+    void UpdateMOVE(GamePadState pad)
+    {        
+        if (!canMove) return;
+        Vector3 direction = Vector3.zero;
+
         if (pad.IsConnected)
         {
             InputSettingsXBOX.Direction d = InputSettingsXBOX.getDirection(game.settings.XboxController.move, pad);
@@ -108,13 +222,15 @@ public class Gamer : MonoBehaviour
                 direction += (Camera.main.transform.right);
             }
         }
-        
+
         if (direction != Vector3.zero)
         {
             controlled.Order = Order.OrderMove(controlled.transform.position + direction.normalized, Order.TYPE_DEPLACEMENT.COURSE);
         }
+    }
 
-        // Drop
+    void UpdateDROP(GamePadState pad)
+    {
         if (pad.IsConnected)
         {
             if (btnDropReleased && InputSettingsXBOX.GetButton(game.settings.XboxController.drop, pad))
@@ -127,130 +243,21 @@ public class Gamer : MonoBehaviour
         {
             controlled.Order = Order.OrderDrop(game.left[0]);
         }
+    }
 
-        // Plaquer
+    void CheckReleased(GamePadState pad)
+    {
         if (pad.IsConnected)
         {
-            if (btnPlaquerReleased && InputSettingsXBOX.GetButton(game.settings.XboxController.plaquer, pad))
+            if (!InputSettingsXBOX.GetButton(game.settings.XboxController.drop, pad))
             {
-                btnPlaquerReleased = false;                
-                if (controlled.NearUnits.Count > 0)
-                {
-                    controlled.Order = Order.OrderPlaquer(controlled.NearUnits[0]);
-                }               
+                btnDropReleased = true;
+            }
+            if (!InputSettingsXBOX.GetButton(game.settings.XboxController.plaquer, pad))
+            {
+                btnPlaquerReleased = true;
             }
         }
-		else if (Input.GetKeyDown(inputs.plaquer) && controlled.NearUnits.Count > 0)
-		{
-			controlled.Order = Order.OrderPlaquer(controlled.NearUnits[0]);
-		}
-
-        // Pass
-        if (game.Ball.Owner == controlled)
-        {
-            if (pad.IsConnected)
-            {
-                if (btnPassReleased && InputSettingsXBOX.GetButton(game.settings.XboxController.passRight, pad))
-                {
-                    btnPassReleased = false;
-
-                    onActionCapture = true;
-                    passDirection = this.transform.right;
-                    game.Ball.transform.position = controlled.BallPlaceHolderRight.transform.position;
-                    unitsSide = controlled.Team.GetRight(controlled);
-                }
-                else if (btnPassReleased && InputSettingsXBOX.GetButton(game.settings.XboxController.passLeft, pad))
-                {
-                    btnPassReleased = false;
-
-                    onActionCapture = true;
-                    passDirection = -this.transform.right;
-                    game.Ball.transform.position = controlled.BallPlaceHolderLeft.transform.position;
-                    unitsSide = controlled.Team.GetLeft(controlled);
-                }
-                else if (!btnPassReleased && !InputSettingsXBOX.GetButton(game.settings.XboxController.passRight, pad) && !InputSettingsXBOX.GetButton(game.settings.XboxController.passLeft, pad))
-                {
-                    btnPassReleased = true;
-
-                    if (controlled == game.Ball.Owner)
-                    {
-                        onActionCapture = false;
-                        if (timeOnActionCapture > game.settings.maxTimeHoldingPassButton)
-                            timeOnActionCapture = game.settings.maxTimeHoldingPassButton;
-                        //Debug.DrawRay(this.transform.position, passDirection, Color.red);
-
-                        if (unitsSide.Count != 0)
-                        {
-                            int unit = Mathf.FloorToInt(unitsSide.Count * timeOnActionCapture / game.settings.maxTimeHoldingPassButton);
-                            Debug.Log(unit);
-
-                            if (unit == unitsSide.Count) unit--;
-                            Unit u = unitsSide[unit];
-
-                            controlled.Order = Order.OrderPass(u);
-                            passDirection = Vector3.zero;
-                        }
-                    }
-                    else
-                    {
-                        // Changer de perso.
-                    }
-                }
-            }
-            else 
-            {
-                if (Input.GetKeyDown(inputs.passRight))
-                {
-                    onActionCapture = true;
-                    passDirection = this.transform.right;
-                    game.Ball.transform.position = controlled.BallPlaceHolderRight.transform.position;
-                    unitsSide = controlled.Team.GetRight(controlled);
-                }
-
-                else if (Input.GetKeyDown(inputs.passLeft))
-                {
-                    onActionCapture = true;
-                    passDirection = -this.transform.right;
-                    game.Ball.transform.position = controlled.BallPlaceHolderLeft.transform.position;
-                    unitsSide = controlled.Team.GetLeft(controlled);
-                }
-
-                else if (Input.GetKeyUp(inputs.passRight) || Input.GetKeyUp(inputs.passLeft))
-                {
-                    if (controlled == game.Ball.Owner)
-                    {
-                        onActionCapture = false;
-                        if (timeOnActionCapture > game.settings.maxTimeHoldingPassButton)
-                            timeOnActionCapture = game.settings.maxTimeHoldingPassButton;
-                        //Debug.DrawRay(this.transform.position, passDirection, Color.red);
-
-                        int unit = Mathf.FloorToInt(unitsSide.Count * timeOnActionCapture / game.settings.maxTimeHoldingPassButton);
-                        if (unitsSide.Count != 0)
-                        {
-                            Debug.Log(unit);
-                            if (unit == unitsSide.Count) unit--;
-                            Unit u = unitsSide[unit];
-
-                            controlled.Order = Order.OrderPass(u);
-                            passDirection = Vector3.zero;
-                        }
-                    }
-                    else
-                    {
-                        // Changer de perso.
-                    }
-                }
-            }
-        }
-
-		if (onActionCapture)
-		{
-			timeOnActionCapture += Time.deltaTime;
-		}
-		else
-		{
-			timeOnActionCapture = 0.0f;
-		}
-	}
+    }
 
 }
