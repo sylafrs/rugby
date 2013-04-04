45add124c4cf2c8956ba053a40caa7426f45a7e8
sylafrs <sylaf@hotmail.fr>
2013-04-04 13:10:43 +0200

    test shader

--- Unity/Assets/Scripts/Game/MiniGames/TransformationManager.cs
+++ Unity/Assets/Scripts/Game/MiniGames/TransformationManager.cs
@@ -15,21 +15,24 @@ public class TransformationManager : MonoBehaviour {
 	public Gamer gamer {get; set;}
 	public Ball ball {get; set;}
 	
-	//public InputDirection direction;
 	public InputTouch touch;
 	
 	private Quaternion initialRotation;
 	
 	private float angle = 0;
 	public float angleSpeed;
-	//public float angleSpeedKeyBoard;
-	//public float angleSpeedReleasedKeyBoard;
 	
 	private float power = 0;
 	public float powerSpeed;
 	
 	public Vector3 maxPower;
 	public float maxAngle;
+	
+	public bool infiniteTime = true;
+	public float timeAngle = 0;
+	public float timePower = 0;
+	
+	private float remainingTime = 0;
 	
 	private enum State {
 		ANGLE,
@@ -42,69 +45,38 @@ public class TransformationManager : MonoBehaviour {
 	private State state;
 	
 	public GameObject arrow;
-	
-	public void Start() {
-		GameObject arr = GameObject.Instantiate(arrow) as GameObject;
+	private GameObject myArrow;
 		
-		arr.transform.parent = this.transform;
-		arr.transform.localPosition = Vector3.zero;
-		arr.transform.localRotation = Quaternion.identity;
-		arr.transform.localScale = Vector3.one;
-		
-		this.arrow = arr;
-	}
-	
 	public void OnEnable() {
 		angle = 0;
 		power = 0;
 		initialRotation = ball.Owner.transform.rotation;
-		this.state = State.ANGLE;			
+		
+		this.remainingTime = timeAngle;
+		this.state = State.ANGLE;
+		
+		myArrow = GameObject.Instantiate(arrow) as GameObject;
+		if(!myArrow)
+			throw new UnityException("Erreur : missing arrow");
+		
+		myArrow.transform.parent = ball.Owner.transform;
+		myArrow.transform.localPosition = Vector3.zero;
+		myArrow.transform.localRotation = Quaternion.identity;
+		
 	}
 	
 	public void OnGUI() {
 		GUILayout.Space(300);
 		GUILayout.Label ("Transformation");
 		GUILayout.Label ("State : " + state);
+		GUILayout.Label ("Time : " + (infiniteTime ? "Infinite" : remainingTime.ToString()));
 		GUILayout.Label ("Angle : " + angle);
 		GUILayout.Label ("Power : " + power);
 	}
 	
 	public void Update() {
 		
-		if(state == State.ANGLE) {
-			/*if(gamer.XboxController.IsConnected) {		
-				angle = gamer.XboxController.GetDirection(direction.xbox).x;
-				
-				if(gamer.XboxController.GetButtonDown(touch.xbox)) {
-					state = State.POWER;	
-				}
-			}
-			else {
-				float r = direction.keyboard.GetRight();
-				if(r == 0) {
-					if(angle > -0.01 && angle < 0.01) {
-						r = 0; 
-						angle = 0;
-					}
-					else {
-						if(angle > 0) 
-							r = -angleSpeedReleasedKeyBoard;
-						if(angle < 0) 
-							r = angleSpeedReleasedKeyBoard;
-					}
-				}
-				
-				angle += (Time.deltaTime * angleSpeedKeyBoard * r);			
-				if(angle < -1)
-					angle = -1;
-				if(angle > 1)
-					angle = 1;
-				
-				if(Input.GetKeyDown(touch.keyboard)) {
-					state = State.POWER;	
-				}
-			}*/
-			
+		if(state == State.ANGLE) {				
 			angle += angleSpeed * Time.deltaTime;
 			if(angle > maxAngle) {
 				angle = maxAngle;
@@ -115,16 +87,20 @@ public class TransformationManager : MonoBehaviour {
 				angleSpeed *= -1;
 			}
 			
-			
-			if(Input.GetKeyDown(touch.keyboard) || (gamer.XboxController.IsConnected && gamer.XboxController.GetButtonDown(touch.xbox))) {
+			if(!infiniteTime) {
+				remainingTime -= Time.deltaTime;	
+			}				
+				
+			if(remainingTime < 0 || Input.GetKeyDown(touch.keyboard) || (gamer.XboxController.IsConnected && gamer.XboxController.GetButtonDown(touch.xbox))) {
+				remainingTime = timePower;
 				state = State.POWER;	
 			}
 			
-			ball.Owner.transform.rotation = initialRotation * Quaternion.Euler(new Vector3(0, angle * maxAngle, 0));
+			ball.Owner.transform.rotation = initialRotation * Quaternion.Euler(new Vector3(0, angle, 0));
 		}
 		
 		if(state == State.POWER) {
-			
+					
 			power += powerSpeed * Time.deltaTime;
 			if(power > 1) {
 				power = 1;
@@ -135,9 +111,15 @@ public class TransformationManager : MonoBehaviour {
 				powerSpeed *= -1;
 			}
 			
-			if((gamer.XboxController.IsConnected && gamer.XboxController.GetButtonUp(touch.xbox)) || Input.GetKeyUp(touch.keyboard)) {
+			if(!infiniteTime) {
+				remainingTime -= Time.deltaTime;	
+			}
+			
+			if(remainingTime < 0 || (gamer.XboxController.IsConnected && gamer.XboxController.GetButtonUp(touch.xbox)) || Input.GetKeyUp(touch.keyboard)) {
 				Launch();
 			}
+			
+			
 		}
 		
 		if(state == State.WAITING) {
@@ -161,22 +143,21 @@ public class TransformationManager : MonoBehaviour {
 	public void Launch() {
 		
 		state = State.WAITING;	
+		
+		GameObject.Destroy(myArrow);
 					
 		ball.transform.parent = null;
         ball.rigidbody.useGravity = true;
 		ball.rigidbody.isKinematic = false;
-    //  ball.rigidbody.AddForce(Owner.transform.forward * angle * maxAngle + Owner.transform.up * power * maxPower + Owner.transform.right * power * maxPower);
         
-		ball.Owner.transform.rotation = initialRotation * Quaternion.Euler(new Vector3(0, angle * maxAngle, 0));
+		ball.Owner.transform.rotation = initialRotation * Quaternion.Euler(new Vector3(0, angle, 0));
 			
 		Vector3 force = ball.Owner.transform.forward * power * maxPower.x + 
 						ball.Owner.transform.right * power * maxPower.z +
 						ball.Owner.transform.up * power * maxPower.y;
 		
 		ball.rigidbody.AddForce(force);
-		
-		//Debug.DrawRay(ball.transform.position, force, Color.red, 100);
-		
+				
 		ball.Owner = null;
 	}
 	
