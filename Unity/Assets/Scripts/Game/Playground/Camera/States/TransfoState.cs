d3a9525a92b9f228545e64c6a98f266f42a1a80b
Maxens Dubois <maxens.dubois@gmail.com>
2013-05-03 12:46:02 +0200

    Signed-off-by: Maxens Dubois <maxens.dubois@gmail.com>

--- Unity/Assets/Scripts/Game/Playground/Camera/States/TransfoState.cs
+++ Unity/Assets/Scripts/Game/Playground/Camera/States/TransfoState.cs
@@ -4,13 +4,27 @@
   * @author Sylvain Lafon
   * @see CameraState
   */
+using System.Collections.Generic;
+using UnityEngine;
+
 public class TransfoState : CameraState
 {
     public TransfoState(StateMachine sm, CameraManager cam) : base(sm, cam) { }
 	
 	public override void OnEnter ()
 	{
-	
-	}
+		cam.setTarget(null);
+		
+		Vector3 offset = Camera.mainCamera.transform.position+(cam.MinfollowOffset)*cam.zoom;
+		
+		cam.transalateToWithFade(cam.game.Ball.Owner.transform.position - offset, cam.game.Ball.Owner.transform.rotation, 0f, 1f, 2.5f, 0.5f, 
+            (/* OnFinish */) => {
+                //please, kill after usage x)
+                CameraFade.wannaDie();
+            }, (/* OnFade */) => {
+                cam.setTarget(cam.game.Ball.Owner.transform);
+            }
+        );
+	}	
 	
 }
