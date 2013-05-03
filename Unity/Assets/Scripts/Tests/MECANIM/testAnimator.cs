462b4cbedd269e82e56e89d11fab0e7c3371467f
sylafrs <sylaf@hotmail.fr>
2013-05-03 17:23:07 +0200

    But bleu ~prefab. Tests/MECANIM
    Signed-off-by: sylafrs <sylaf@hotmail.fr>

---
+++ Unity/Assets/Scripts/Tests/MECANIM/testAnimator.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+/**
+  * @class testAnimator
+  * @brief Description.
+  * @author Sylvain Lafon
+  * @see MonoBehaviour
+  */
+public class testAnimator : MonoBehaviour {
+   
+}
