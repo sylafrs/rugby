ddd4d7fa137ccff13d5c26a49a8cfe1ca520edc2
Maxens Dubois <maxens.dubois@gmail.com>
2013-05-03 14:07:47 +0200

    V1 de la cam tranfo
    Signed-off-by: Maxens Dubois <maxens.dubois@gmail.com>

--- Unity/Assets/Scripts/Game/Playground/Camera/States/TransfoState.cs
+++ Unity/Assets/Scripts/Game/Playground/Camera/States/TransfoState.cs
@@ -4,7 +4,6 @@
   * @author Sylvain Lafon
   * @see CameraState
   */
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TransfoState : CameraState
@@ -15,14 +14,16 @@ public class TransfoState : CameraState
 	{
 		cam.setTarget(null);
 		
-		Vector3 offset = Camera.mainCamera.transform.position+(cam.MinfollowOffset)*cam.zoom;
+		Transform cameraPlaceHolder = GameObject.Find("TransfoPlacement").transform.FindChild("ShootPlayer").
+			transform.FindChild("CameraPlaceHolder");
+		cameraPlaceHolder.LookAt(cam.game.Ball.Owner.transform);
 		
-		cam.transalateToWithFade(cam.game.Ball.Owner.transform.position - offset, cam.game.Ball.Owner.transform.rotation, 0f, 1f, 2.5f, 0.5f, 
+		cam.transalateToWithFade(cameraPlaceHolder.position, cameraPlaceHolder.rotation, 0f, 1f, 1f, 0.5f, 
             (/* OnFinish */) => {
                 //please, kill after usage x)
                 CameraFade.wannaDie();
             }, (/* OnFade */) => {
-                cam.setTarget(cam.game.Ball.Owner.transform);
+                //cam.setTarget(cam.game.Ball.Owner.transform);
             }
         );
 	}	
