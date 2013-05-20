ba181a01d3178b29015760d8c2c1201e9d91525b
sylafrs <sylaf@hotmail.fr>
2013-05-20 15:09:27 +0200

    liens main

--- Unity/Assets/Scripts/Game/Playground/Game/States/MainGameState.cs
+++ Unity/Assets/Scripts/Game/Playground/Game/States/MainGameState.cs
@@ -2,31 +2,27 @@ using UnityEngine;
 
 /**
   * @class MainGameState
-  * @brief Etat principal de la caméra.
+  * @brief Etat principal du jeu.
   * @author Sylvain Lafon
   * @see GameState
   */
 public class MainGameState : GameState {
 
     public MainGameState(StateMachine sm, CameraManager cam, Game game) : base(sm, cam, game) { }
-	
-	public override bool OnIntroLaunch()
+
+    public override void OnEnter()
     {
-		Debug.Log("check intro start");
-		sm.state_change_son(this, new IntroState(sm, cam, game));
-		return true;
-	}
-	
-	public override bool OnEndLaunch()
+        sm.state_change_son(this, new IntroState(sm, cam, game));
+    }
+
+    public override bool OnEndSignal()
     {
-		sm.state_change_son(this, new EndState(sm, cam, game));
-		return true;
-	}
+        sm.state_change_son(this, new EndState(sm, cam, game));
+        return true;
+    }
 	
-	public override bool OnIntroEnd()
+	public override bool OnStartSignal()
     {
-		//sm.state_change_son(this, new WaitingState(sm, cam, game));
-		Debug.Log("check intro end");
 		sm.state_change_son(this, new PlayingState(sm, cam, game));
 		return true;
 	}
