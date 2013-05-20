b90bfb9384a1b166a271c12af8075828a1b24c8c
sylafrs <sylaf@hotmail.fr>
2013-05-20 19:01:28 +0200

    gameAction
    Signed-off-by: sylafrs <sylaf@hotmail.fr>

---
+++ Unity/Assets/Scripts/Game/Global/States/GameActionState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+/**
+  * @class GameActionState
+  * @brief Description.
+  * @author Sylvain Lafon
+  * @see MonoBehaviour
+  */
+public class GameActionState : GameState {
+
+    public GameActionState(StateMachine sm, CameraManager cam, Game game) : base(sm, cam, game) { }
+
+    public override bool OnTouch()
+    {
+        sm.state_change_son(this, new TouchState(sm, cam, game));
+        return true;
+    }
+
+}
