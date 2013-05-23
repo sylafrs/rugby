0544a9ad315926a8121d48b269ee7b42b5215c3a
Florian <Florian.guilleminot@gmail.com>
2013-05-23 15:37:49 +0200

    other fix
    Signed-off-by: Florian <Florian.guilleminot@gmail.com>

--- Unity/Assets/Scripts/Game/Global/States/PlayingState.cs
+++ Unity/Assets/Scripts/Game/Global/States/PlayingState.cs
@@ -61,18 +61,5 @@ public class PlayingState : GameState
         game.refs.managers.ui.currentState = UIManager.UIState.NULL;
 	}
 
-
-	public override void OnUpdate()
-	{
-		foreach (Unit u in game.northTeam)
-		{
-			u.UpdatePlacement();
-		}
-
-		foreach (Unit u in game.southTeam)
-		{
-			u.UpdatePlacement();
-		}
-	}
 	
 }
