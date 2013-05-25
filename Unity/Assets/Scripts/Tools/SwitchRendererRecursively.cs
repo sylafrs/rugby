e994ef3c1242fa3f4dce750a9f3d6e8d162fc633
sylafrs <sylaf@hotmail.fr>
2013-05-25 20:31:45 +0200

    enable/disable renderers recursively button (files)

---
+++ Unity/Assets/Scripts/Tools/SwitchRendererRecursively.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+/**
+  * @class SwitchRendererRecursively
+  * @brief Description.
+  * @author Sylvain Lafon
+  * @see MonoBehaviour
+  */
+public class SwitchRendererRecursively : MonoBehaviour {
+
+}
