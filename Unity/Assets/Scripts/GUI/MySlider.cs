73a14ab0de7bc9dc888036173607b5fdcade8d15
sylafrs <sylaf@hotmail.fr>
2013-06-18 12:49:07 +0200

    ui

--- Unity/Assets/Scripts/GUI/MySlider.cs
+++ Unity/Assets/Scripts/GUI/MySlider.cs
@@ -8,15 +8,40 @@ using System.Collections.Generic;
   * @see MonoBehaviour
   */
 public class MySlider : MonoBehaviour {
+    public UITexture bar;
+    public UITexture empty;
     public UITexture full;
+    public UITexture fullIndicator;
+
     public float fullScaleZ;
+    public float emptyScaleZ;
 
     public float percent;
 
     public void Update()
     {
-        Vector3 s = full.transform.localScale;
-        s.x = Mathf.Clamp01(percent) * fullScaleZ;
-        full.transform.localScale = s;
+        if (bar != null)
+        {
+            Vector3 s = bar.transform.localScale;
+            s.x = Mathf.Clamp01(percent) * fullScaleZ;
+            bar.transform.localScale = s;
+        }
+
+        if (full != null)
+        {
+            full.gameObject.SetActive(Mathf.Clamp01(percent) == 1);
+        }
+
+        if (fullIndicator != null)
+        {
+            fullIndicator.gameObject.SetActive(Mathf.Clamp01(percent) == 1);
+        }
+
+        if (empty != null)
+        {
+            Vector3 s = empty.transform.localScale;
+            s.x = Mathf.Clamp01(1 - percent) * emptyScaleZ;
+            empty.transform.localScale = s;
+        }
     }
 }
