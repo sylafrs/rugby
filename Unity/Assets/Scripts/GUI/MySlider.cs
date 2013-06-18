dd75e86aa930482ff9957f96c7bc72e3efe61315
sylafrs <sylaf@hotmail.fr>
2013-06-18 19:33:15 +0200

    better scale

--- Unity/Assets/Scripts/GUI/MySlider.cs
+++ Unity/Assets/Scripts/GUI/MySlider.cs
@@ -7,11 +7,12 @@ using System.Collections.Generic;
   * @author Sylvain Lafon
   * @see MonoBehaviour
   */
-public class MySlider : MonoBehaviour {
+public class MySlider : MonoBehaviour
+{
     public UITexture bar;
     public UITexture empty;
     public UITexture full;
-    public UITexture [] fullIndicators;
+    public UITexture[] fullIndicators;
 
     public float fullScaleZ;
     public float emptyScaleZ;
@@ -20,16 +21,27 @@ public class MySlider : MonoBehaviour {
 
     public void Update()
     {
+        float p = Mathf.Clamp01(percent);
+
         if (bar != null)
         {
             Vector3 s = bar.transform.localScale;
-            s.x = Mathf.Clamp01(percent) * fullScaleZ;
+            s.x = p * fullScaleZ;
             bar.transform.localScale = s;
+
+            if (bar.pivot == UIWidget.Pivot.Left)
+            {
+                bar.uvRect = new Rect(0, 0, p, 1);
+            }
+            if (empty.pivot == UIWidget.Pivot.Right)
+            {
+                bar.uvRect = new Rect(1 - p, 0, p, 1);
+            }
         }
 
         if (full != null)
         {
-            full.gameObject.SetActive(Mathf.Clamp01(percent) == 1);
+            full.gameObject.SetActive(p == 1);
         }
 
         if (fullIndicators != null)
@@ -38,7 +50,7 @@ public class MySlider : MonoBehaviour {
             {
                 if (fullIndicator != null)
                 {
-                    fullIndicator.gameObject.SetActive(Mathf.Clamp01(percent) == 1);
+                    fullIndicator.gameObject.SetActive(p == 1);
                 }
             }
         }
@@ -46,8 +58,17 @@ public class MySlider : MonoBehaviour {
         if (empty != null)
         {
             Vector3 s = empty.transform.localScale;
-            s.x = Mathf.Clamp01(1 - percent) * emptyScaleZ;
+            s.x = (1 - p) * emptyScaleZ;
             empty.transform.localScale = s;
+
+            if (empty.pivot == UIWidget.Pivot.Left)
+            {
+                empty.uvRect = new Rect(0, 0, p, 1);
+            }
+            if (empty.pivot == UIWidget.Pivot.Right)
+            {
+                empty.uvRect = new Rect(1 - p, 0, p, 1);
+            }
         }
     }
 }
