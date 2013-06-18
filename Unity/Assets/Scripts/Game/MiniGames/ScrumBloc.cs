756d7e2f0868c63397687d5891111cd5e0c5b664
sylafrs <sylaf@hotmail.fr>
2013-06-18 17:20:30 +0200

    scrum bloc script

---
+++ Unity/Assets/Scripts/Game/MiniGames/ScrumBloc.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+/**
+  * @class ScrumBloc
+  * @brief Description.
+  * @author Sylvain Lafon
+  * @see MonoBehaviour
+  */
+public class ScrumBloc : MonoBehaviour {
+    public Animator north;
+    public Animator south;
+}
