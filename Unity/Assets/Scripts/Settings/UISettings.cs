b3103737a1f2ee5974da10337f348cfb35203828
sylafrs <sylaf@hotmail.fr>
2013-06-18 15:26:46 +0200

    ui

--- Unity/Assets/Scripts/Settings/UISettings.cs
+++ Unity/Assets/Scripts/Settings/UISettings.cs
@@ -14,6 +14,9 @@ public class GameUISettings
 {
     public UIPanel MainPanel;
 
+    public MySlider blueSuper;
+    public MySlider redSuper;
+
 	//for super bar
 	public Texture2D emptyBar;
 	public Texture2D blueBar;
@@ -31,18 +34,16 @@ public class GameUISettings
 	public float timeBoxHeightPercentage  = 5;
 	public float timeBoxXPercentage		= 50;
 	public float timeBoxYPercentage		= 10;
-
-    public MySlider blueSuper;
-
+    
 	//public float blueGaugeBoxWidthPercentage   = 25;
 	//public float blueGaugeBoxHeightPercentage  = 10;
 	//public float blueGaugeBoxXPercentage		= 22.5f;
 	//public float blueGaugeBoxYPercentage		= 10;
 	
-	public float redGaugeBoxWidthPercentage   = 25;
-	public float redGaugeBoxHeightPercentage  = 10;
-	public float redGaugeBoxXPercentage		= 77.5f;
-	public float redGaugeBoxYPercentage		= 10;
+	//public float redGaugeBoxWidthPercentage   = 25;
+	//public float redGaugeBoxHeightPercentage  = 10;
+	//public float redGaugeBoxXPercentage		= 77.5f;
+	//public float redGaugeBoxYPercentage		= 10;
 	
 	public float scoreBoxWidthPercentage  = 20;
 	public float scoreBoxHeightPercentage = 15;
