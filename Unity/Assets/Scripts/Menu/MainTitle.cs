6fd15d17c5cff15e0f043524e2022d085a72286d
sylafrs <sylaf@hotmail.fr>
2013-06-19 11:01:43 +0200

    menu

--- Unity/Assets/Scripts/Menu/MainTitle.cs
+++ Unity/Assets/Scripts/Menu/MainTitle.cs
@@ -9,19 +9,24 @@ using System.Collections.Generic;
   */
 public class MainTitle : MonoBehaviour {
     public InputTouch touch;
+    public InputTouch skip;
 
     public float timeBeforeLoading;
     public float timeBeforeFading;
     public UITexture background;
     public UITexture pushStart;
     public MyFade fade;
+    public MySprite AButton;
+
+    public AudioSource validation;
 
     private XboxInputs inputs;
     
     enum Status
     {
         TITLE,
-        CONTROLS
+        CONTROLS,
+        GOOUT
     }
 
     private Status state;
@@ -64,15 +69,55 @@ public class MainTitle : MonoBehaviour {
                 pushStart.gameObject.SetActive(false);
                 state = Status.CONTROLS;
 
+                AButton.gameObject.SetActive(true);
+                validation.Play();
+
                 Timer.AddTimer(timeBeforeFading, () =>
                 {
-                    fade.Inverse();
-                });
-                Timer.AddTimer(timeBeforeLoading, () =>
-                {
-                    Application.LoadLevel("terrain");
+                    GoOut();
                 });
             }
+        }
+        else
+        {
+            bool ok = false;
+            foreach (var c in inputs.controllers)
+            {
+                if (c.GetButtonDown(skip.xbox))
+                {
+                    ok = true;
+                    break;
+                }
+            }
+
+            if (!ok)
+            {
+                if (Input.GetKeyDown(skip.keyboardP1) || Input.GetKeyDown(skip.keyboardP2))
+                {
+                    ok = true;
+                }
+            }
+
+            if (ok)
+            {
+                GoOut();
+            }
+        }
+    }
+
+    void GoOut()
+    {
+        if (state != Status.GOOUT)
+        {
+            AButton.gameObject.SetActive(false);
+            fade.Inverse();
+
+            Timer.AddTimer(timeBeforeLoading, () =>
+            {
+                Application.LoadLevel("terrain");
+            });
+
+            state = Status.GOOUT;
         }
     }
 }
